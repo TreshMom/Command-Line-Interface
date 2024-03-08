9e0d4cca0378223576dd3ca7c82cca07cdfffbf6
TreshMom <selfpitying@gmail.com>
2024-03-09 00:01:05 +0300

    Wc and Cat release

--- project/src/mse/sd/bash/commands/Cat.java
+++ project/src/mse/sd/bash/commands/Cat.java
@@ -1,4 +1,17 @@
 package mse.sd.bash.commands;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Cat {
+    public static void cat(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- project/src/mse/sd/bash/commands/Wc.java
+++ project/src/mse/sd/bash/commands/Wc.java
@@ -1,4 +1,29 @@
 package mse.sd.bash.commands;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.nio.charset.Charset;
 
 public class Wc {
+    public static void wc(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            int lines = -1;
+            int words = 0;
+            int bytes = 0;
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines++;
+                bytes += line.getBytes(Charset.forName("UTF-8")).length; // проблемы
+                words += new StringTokenizer(line, " ").countTokens();
+            }
+
+            System.out.println("Lines: " + lines);
+            System.out.println("Words: " + words);
+            System.out.println("Bytes: " + bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
