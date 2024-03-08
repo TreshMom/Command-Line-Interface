2da898abf9e619b7480d06f3b82ad61f2a318783
Menzorov_Konstatin <mnz.const@gmail.com>
2024-03-09 02:30:30 +0300

    add echo and pwd

--- project/src/mse/sd/bash/commands/Echo.java
+++ project/src/mse/sd/bash/commands/Echo.java
@@ -1,8 +1,15 @@
 package mse.sd.bash.commands;
 
 public class Echo extends Command {
+
+    private final String str;
+
+    public Echo(String str) {
+        this.str = str;
+    }
+
     @Override
     public void eval() {
-
+        System.out.println(str);
     }
 }
--- project/src/mse/sd/bash/commands/Pwd.java
+++ project/src/mse/sd/bash/commands/Pwd.java
@@ -1,4 +1,8 @@
 package mse.sd.bash.commands;
 
-public class Pwd {
+public class Pwd extends Command {
+    @Override
+    public void eval() {
+        System.out.println(System.getProperty("user.dir"));
+    }
 }
